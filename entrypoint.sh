90df7cc512db1491e4d5b396f80dde51e6a99f4f
João Canoso <jpcanoso@hotmail.com>
2021-01-18 01:35:35 +0000

    Changed vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 cmd="$@"
   
-until curl -i -s -f -o /dev/null -u $INPUT_USER:$INPUT_PASS $INPUT_URL; do
-  >&2 echo "Service is not ready - waiting $INPUT_TIME s"
-  sleep $INPUT_TIME
+until curl -i -s -f -o /dev/null -u $INPUT_WAIT_USER:$INPUT_WAIT_PASS $INPUT_WAIT_URL; do
+  >&2 echo "Service is not ready - waiting $INPUT_WAIT_TIME s"
+  sleep $INPUT_WAIT_TIME
 done
 
 >&2 echo "Service is ready"
