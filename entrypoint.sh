5a7d20b44844138b5f4ad2cc1221621ef962aca8
João Canoso <jpcanoso@hotmail.com>
2021-01-18 00:27:41 +0000

    Added main script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh -l
 
-echo "hello $1"
+set -e
+
+cmd="$@"
+  
+until curl -i -s -f -o /dev/null -u $INPUT_USER:$INPUT_PASS $INPUT_URL; do
+  >&2 echo "Service is not ready - waiting $INPUT_TIME"
+  sleep $INPUT_TIME
+done
+
+>&2 echo "Service is ready"
+
+exit 0
