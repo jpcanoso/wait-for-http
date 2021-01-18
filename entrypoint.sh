1fcf11de9ddb5a111006e011a34da5886a7c39fb
João Canoso <jpcanoso@hotmail.com>
2021-01-18 14:23:09 +0000

    simplified script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh -l
 
-set -e
-  
-until curl -i -s -f -o /dev/null -u $INPUT_WAIT_USER:$INPUT_WAIT_PASS $INPUT_WAIT_URL; do
-  >&2 echo "Service is not ready - waiting $INPUT_WAIT_TIME s"
-  sleep $INPUT_WAIT_TIME
+until curl -i -s -f -o /dev/null -u ${INPUT_WAIT_USER}:${INPUT_WAIT_PASS} ${INPUT_WAIT_URL}; do
+  echo "Service is not ready - waiting ${INPUT_WAIT_TIME} s"
+  sleep ${INPUT_WAIT_TIME}
 done
 
->&2 echo "Service is ready"
+echo "Service is ready"
 
 exit 0
